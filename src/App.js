226367a8cafc266448bbaec19d4c676c034807ff
Andrey-Pidgorodetsky <andrey2007@ukr.net>
2021-12-19 02:17:43 +0200

    add-dinamical-lists

--- src/App.js
+++ src/App.js
@@ -3,19 +3,72 @@ import './App.css';
 import Car from './Car/Car';
 
 class App extends Component {
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018 },
+      {name:'Audi', year: 2016 },
+      {name: 'Mazda', year: 2010 }
+    ],
+    pageTitle: 'React component',
+    showCars: false
+  }
+
+  onChangeName(name, index) {
+    console.log(name , index)
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState ({
+      cars: cars
+    })
+  }
+
+  toogleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    })
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+
+  }
+
+
+
   render() {
+    console.log('render')
     const divStyle={
-      'text-align': 'center',
+      'textAlign': 'center',
     }
+
+    const cars = this.state.cars 
+
     return(
       <div  style={divStyle}>
-        <h1>Hello World</h1>
+        <h1>{this.state.pageTitle}</h1>
+        <button 
+          onClick={this.toogleCarsHandler}
+          >Toogle Cars</button>
 
-        <Car name={'Ford'} year={2018}></Car>
-          <p style={{color: 'Blue'}}>Color</p>
-        <Car name={'Audi'} year={2016}></Car>
-          <p style={{color: 'red'}}>Color</p>
-        <Car name={'Mazda'} year={2010}></Car>
+        { this.state.showCars ?
+            this.state.cars.map((car, index) => {
+              return (
+                <Car 
+                  key={index}
+                  name={car.name}
+                  year={car.year}
+                  onDelete={this.deleteHandler.bind(this, index)}
+                  onChangeName={event => this.onChangeName(event.target.value, index)}
+                ></Car>
+              )
+            })
+            : null
+        }       
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+
+
 export default car =>  (
-    <div>
+    <div style={{
+        border: '1px solid #ccc',
+        marginBottom: '10px',
+        display: 'block',
+        padding: '10px'
+    }}>
         <h3>Car name: {car.name}</h3>
         <p>Year: <strong>{car.year}</strong></p>
-        { car.children } 
+        <input type="text" onChange={car.onChangeName} value={car.name}></input>
+        <button onClick={car.onDelete}>Delete</button>
     </div>
 )
